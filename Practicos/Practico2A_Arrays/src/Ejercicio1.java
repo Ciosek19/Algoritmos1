e6ccad49afbb22c6233e8238f5e7f0df8b35b479
Rodrigo <modrips4@gmail.com>
2025-05-08 15:12:38 -0300

    Ejercicio1

--- Practicos/Practico2A_Arrays/src/Ejercicio1.java
+++ Practicos/Practico2A_Arrays/src/Ejercicio1.java
@@ -1,2 +1,20 @@
 public class Ejercicio1 {
+    public static String[][] personas = {
+            {"Juan Rodriguez","Martin Ocampo"},
+            {"Maria Perez", "Igancio Castillo"},
+            {"Carla Gomez", "Ana Romero"},
+    };
+
+    public static void imprimirMatriz(String[][] matriz) {
+        for (int i = 0; i < matriz[i].length; i++) {
+            for (int j = 0; j < matriz[j].length; j++) {
+                System.out.print(matriz[i][j]+ "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        imprimirMatriz(personas);
+    }
 }
