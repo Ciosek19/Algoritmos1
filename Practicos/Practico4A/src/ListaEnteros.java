a90c2e3e1512be0749686168b68317ca98eec960
F7cuG <1507facu@gmail.com>
2025-06-18 21:24:51 -0300

    Terminando el practico4a algoritmos

--- Practicos/Practico4A/src/ListaEnteros.java
+++ Practicos/Practico4A/src/ListaEnteros.java
@@ -27,13 +27,69 @@ public class ListaEnteros {
 
     public void imprimirLista(){
         Nodo aux = cabeza;
-        int contador = 1;
+        int contador = 0;
         while (aux != null){
-            System.out.println(contador + ". Dato: "+ aux.getValor());
+            if (contador == 0) System.out.print("Cabeza:"+ aux.getValor());
+            else{
+                System.out.print(" -> "+ aux.getValor());
+            }
             aux = aux.getSiguiente();
             contador++;
         }
+        System.out.println();
     }
 
     // Practico 4A
+    /*
+        1. Crear una función booleana "pertenece" para validar si existe un dato
+        2. Crear una función que elimine el primer elemento "borrarInicio".
+        3. Crear una función que se llame "retornarCola", que debe retornar una lista nueva con todos los datos menos la cabeza
+        4. Crear una función para insertar un dato al final de la lista.
+    */
+
+    public boolean pertenece(int dato){
+        if (this.estaVacia()) return false;
+        Nodo aux = cabeza;
+        while(aux != null){
+            if (aux.getValor() == dato) return true;
+            aux = aux.getSiguiente();
+        }
+        return false;
+    }
+
+    public void eliminarPrimerElemento(){
+        if (this.estaVacia()) System.out.println("La lista esta vacia");
+        else {
+            Nodo aux = cabeza.getSiguiente();
+            cabeza = aux;
+        }
+    }
+
+    public ListaEnteros retornarCola() {
+        if (this.estaVacia()) return null;
+        ListaEnteros nuevaLista = new ListaEnteros();
+        Nodo aux = this.cabeza.getSiguiente();
+        while(aux != null){
+            nuevaLista.insertarAlIncio(aux.getValor());
+            aux = aux.getSiguiente();
+        }
+        return nuevaLista;
+    }
+
+    public void insertarDatoAlFinal(int dato){
+        if (this.estaVacia()){
+            insertarAlIncio(dato);
+        }
+        else {
+            Nodo nuevoNodo = new Nodo(dato);
+            Nodo aux = this.cabeza;
+            while(aux != null){
+                if (aux.getSiguiente() == null) {
+                    aux.setSiguiente(nuevoNodo);
+                }
+                aux = aux.getSiguiente();
+                System.out.println(aux.getValor());
+            }
+        }
+    }
 }
--- Practicos/Practico4A/src/Main.java
+++ Practicos/Practico4A/src/Main.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         ListaEnteros lista = new ListaEnteros();
@@ -5,7 +7,26 @@ public class Main {
         lista.insertarAlIncio(1);
         lista.insertarAlIncio(4);
         lista.insertarAlIncio(10);
+        lista.insertarAlIncio(117);
+        lista.insertarAlIncio(18);
+        lista.insertarAlIncio(15);
 
+        lista.imprimirLista();
+
+        System.out.println("Pertence el 1 al a lista? " + lista.pertenece(1));
+        System.out.println("Pertence el 4 al a lista? " + lista.pertenece(4));
+        System.out.println("Pertence el 15 al a lista? " + lista.pertenece(15));
+
+        lista.eliminarPrimerElemento();
+        System.out.println("Mostrando lista luego de eliminar");
+        lista.imprimirLista();
+
+        System.out.println("Mostrando lista nueva sin la cabeza");
+        ListaEnteros nuevaLista = lista.retornarCola();
+        nuevaLista.imprimirLista();
+
+        System.out.println("Insertando dato al final:");
+        lista.insertarDatoAlFinal(4);
         lista.imprimirLista();
     }
 }
